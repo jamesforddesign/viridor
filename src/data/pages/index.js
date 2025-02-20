56dbd9e351156e5d4e31632ddf977bb8c53ae346
JTaylor <jtaylor@jfd.co.uk>
2025-02-20 12:18:52 +0000

    build: add panel styling

--- src/data/pages/index.js
+++ src/data/pages/index.js
@@ -51,7 +51,7 @@ module.exports = {
                     `,
                 cy: ``,
             },
-            image: "/placeholder.jpg"
+            image: "/runcorn-town-fc-and-viridor.webp"
         },
         {
             type: "text",
@@ -82,12 +82,12 @@ module.exports = {
             },
             content: {
                 en: `
-                    <p>Viridor is progressing the UK’s largest opportunity to decarbonise the waste sector, through the use of carbon capture at its Runcorn ERF facility, with CO2 being captured and safely transported through the HyNet pipeline and then stored permanently, deep underneath Liverpool Bay.</p>
+                    <p>Viridor is progressing the UK’s largest opportunity to decarbonise the waste sector, through the use of carbon capture at its Runcorn ERF facility, with CO<sub>2</sub> being captured and safely transported through the HyNet pipeline and then stored permanently, deep underneath Liverpool Bay.</p>
                     <div class="[ panel ] [ bg-secondary ] [ text-white ]">
                         <p>The HyNet pipeline is a nationally significant infrastructure project that plans to take captured carbon dioxide from across our region and store it under the sea in Liverpool Bay. Our proposals for carbon capture at Runcorn would connect to the Runcorn Spur Pipeline and wider HyNet network, which is being developed by Eni. </p>
                         <p>You can find out more by visiting the HyNet website, <a class="text-white fw-bold" href="https://hynethub.co.uk/index.php?contentid=982" target="_blank">here</a>.</p>
                     </div>
-                    <p>Viridor Runcorn will become one of the first ERFs in the world to introduce carbon capture – and capture around 900,000 tonnes of CO2 each year. It will also see us create net zero power and heat from our facility, helping to decarbonise industry in the North West and contributing to the government’s target of reaching net zero by 2050.</p>
+                    <p>Viridor Runcorn will become one of the first ERFs in the world to introduce carbon capture – and capture around 900,000 tonnes of CO<sub>2</sub> each year. It will also see us create net zero power and heat from our facility, helping to decarbonise industry in the North West and contributing to the government’s target of reaching net zero by 2050.</p>
                     <p>This consultation invites you to find out more and provide feedback, ahead of a planning application being submitted to Halton Borough Council.</p>
                     `,
                 cy: ``,
@@ -98,35 +98,42 @@ module.exports = {
             type: "text",
             bgColour: "tertiary",
             opacity: "10",
-            invert: true,
             title: {
                 en: "Why we need carbon capture",
                 cy: ""
             },
             content: {
                 en: `
-                    <p>Human activity has been the main driver of climate change since the 1800s. The production of greenhouse gases (including CO2) is the key reason why the earth is warming faster than ever before, and extreme weather events are becoming more common.</p>
+                    <p>Human activity has been the main driver of climate change since the 1800s. The production of greenhouse gases (including CO<sub>2</sub>) is the key reason why the earth is warming faster than ever before, and extreme weather events are becoming more common.</p>
                     <p>Waste is a major contributor to greenhouse gas emissions and even with improvements in recycling, the average person still throws away around 500kg of non-recycled waste each year. This is the weight of a small car! </p>
-                    <p>However, our industry is well suited to capturing CO2 and it could have a big impact. For example, we will capture the equivalent carbon to that which is emitted on 3000 commercial flights from London to New York! </p>
+                    <p>However, our industry is well suited to capturing CO<sub>2</sub> and it could have a big impact. For example, we will capture the equivalent carbon to that which is emitted on 3000 commercial flights from London to New York! </p>
                     <p>This project will make our site climate positive by producing negative emissions. This is where the total carbon we capture is more than the total fossil (man-made) carbon we produce. We will be capturing natural carbon so, so we’ll actually be removing carbon from the atmosphere and in doing so we’ll be helping this region to be at the forefront of e¬fforts to reduce the UK’s carbon emissions and reach net zero targets. You can find out more about negative emissions from the Coalition for Negative Emissions, <a class="fw-bold" href="https://coalitionfornegativeemissions.org/" target="_blank">here</a>. </p>
-                    <div class="[ panel ] [ bg-secondary ] [ text-white ]">
-                        <h3>How does carbon capture work</h3>
-                        <ol>
-                            <li>Flue gas is diverted from the existing ERF stack into the proposed carbon capture infrastructure.</li>
-                            <li>The flue gas is then passed through a piece of machinery called a scrubber which cleans it and cools it down. </li>
-                            <li>The gas then moves to the absorber column, where the CO2 is absorbed from the flue gas (while cold) by a CO2 solvent (e.g. amine). The flue gas (minus the CO2) is vented through a stack into the atmosphere after being re-heated. </li>
-                            <li>The CO2 rich solvent is then warmed up through a heat exchanger. A reclaimer is also used to continuously clean the solvent to keep it effective at removing as much CO2 as possible. </li>
-                            <li>In a stripper, the solvent is further heated until it released the CO2 as a pure gaseous stream. </li>
-                            <li>The CO2 is then passed into a compressor, which increases the pressure to pump it away from site. Before entering the transport system, the CO2 is conditioned / treated to ensure it will not corrode the downstream system. </li>
-                            <li>The CO2 captured at Viridor Runcorn is then handed over to the transport and storage operator as part of the HyNet Network, which would take the CO2 for permanent sequestration below impermeable rock.</li>
-                        </ol>
-                    </div>
-                    <p>You can find our more about carbon capture from the Carbon Capture, Utilisations and Storage Association, <a class="fw-bold" href="https://www.ccsassociation.org/" target="_blank">here</a>.</p>
+
 
                     `,
                 cy: ``,
             },
-            image: "/placeholder.jpg"
+
+            contentInvert: {
+                en: `
+                    <div class="[ panel panel--no-padding ]  [ text-white ]">
+                        <h3 class="[ panel__heading bg-primary  ]">How does carbon capture work</h3>
+                        <div class="[ panel__description bg-primary-opacity  ]">
+                            <ol>
+                                <li>Flue gas is diverted from the existing ERF stack into the proposed carbon capture infrastructure.</li>
+                                <li>The flue gas is then passed through a piece of machinery called a scrubber which cleans it and cools it down. </li>
+                                <li>The gas then moves to the absorber column, where the CO<sub>2</sub> is absorbed from the flue gas (while cold) by a CO<sub>2</sub> solvent (e.g. amine). The flue gas (minus the CO<sub>2</sub>) is vented through a stack into the atmosphere after being re-heated. </li>
+                                <li>The CO<sub>2</sub> rich solvent is then warmed up through a heat exchanger. A reclaimer is also used to continuously clean the solvent to keep it effective at removing as much CO<sub>2</sub> as possible. </li>
+                                <li>In a stripper, the solvent is further heated until it released the CO<sub>2</sub> as a pure gaseous stream. </li>
+                                <li>The CO<sub>2</sub> is then passed into a compressor, which increases the pressure to pump it away from site. Before entering the transport system, the CO<sub>2</sub> is conditioned / treated to ensure it will not corrode the downstream system. </li>
+                                <li>The CO<sub>2</sub> captured at Viridor Runcorn is then handed over to the transport and storage operator as part of the HyNet Network, which would take the CO<sub>2</sub> for permanent sequestration below impermeable rock.</li>
+                            </ol>
+                        </div>
+                    </div>
+                    <p>You can find our more about carbon capture from the Carbon Capture, Utilisations and Storage Association, <a class="fw-bold" href="https://www.ccsassociation.org/" target="_blank">here</a>.</p>
+                `,
+            cy: ``,
+            },
         }
     ]
 };
